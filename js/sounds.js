0875f7e4a3da8d4169920afaca2fe94135f9bd44
m2rcos <m2rcosferreira@gmail.com>
2022-06-03 11:56:21 -0300

    file_path

--- js/sounds.js
+++ js/sounds.js
@@ -6,10 +6,10 @@ export default function () {
     'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true'
   )
 
-  const buttonFire = new Audio('../files/Lareira.wav')
-  const buttonRain = new Audio('./../files/Chuva.wav?raw=true')
-  const buttonForrest = new Audio('./../files/Floresta.wav?raw=true')
-  const buttonCoffee = new Audio('./../files/Cafeteria.wav?raw=true')
+  const buttonFire = new Audio('./files/Lareira.wav')
+  const buttonRain = new Audio('./files/Chuva.wav')
+  const buttonForrest = new Audio('./files/Floresta.wav')
+  const buttonCoffee = new Audio('./files/Cafeteria.wav')
 
   buttonFire.loop = true
   buttonRain.loop = true
